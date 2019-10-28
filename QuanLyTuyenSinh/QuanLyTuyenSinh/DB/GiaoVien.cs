1f0f8b81f4859d8ff8399aa6b95549a4f5041f5b
kiên <manhkien13041997@gmail.com>
2019-10-28 21:23:49 +0700

    chuc vu va giao vien moi them

---
+++ QuanLyTuyenSinh/QuanLyTuyenSinh/DB/GiaoVien.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecec.DB
+{
+    class GiaoVien
+    {
+        public static DataTable GiaoVien_GetAll()
+        {
+            string queryString = "SELECT * FROM giaovien";
+            SqlCommand command = new SqlCommand(queryString);
+            return Database.GetData(command);
+        }
+        public static int GiaoVien_Insert(string hoten, string email, string ngaysinh, string sdt, string gioitinh, string chucvu)
+        {
+            string queryString = "insert into giaovien values(@hoten,@ngaysinh,@gioitinh,@email,@sdt,@chucvu)";
+            SqlCommand command = new SqlCommand(queryString);
+            command.Parameters.AddWithValue("@hoten",hoten);
+            command.Parameters.AddWithValue("@ngaysinh",ngaysinh);
+            command.Parameters.AddWithValue("@gioitinh",gioitinh);
+            command.Parameters.AddWithValue("@email",email);
+            command.Parameters.AddWithValue("@sdt",sdt);
+            command.Parameters.AddWithValue("@chucvu",chucvu);
+            return Database.ExecuteNoneQuery(command);
+        }
+        public static int GiaoVien_Update(string id,string hoten, string email, string ngaysinh, string sdt, string gioitinh, string chucvu)
+        {
+            string queryString = "update giaovien set hoten = @hoten,ngaysinh = @ngaysinh,gioitinh = @gioitinh,email = @email,sdt = @sdt,machucvu = @chucvu where magiaovien = @id";
+            SqlCommand command = new SqlCommand(queryString);
+            command.Parameters.AddWithValue("@hoten", hoten);
+            command.Parameters.AddWithValue("@ngaysinh", ngaysinh);
+            command.Parameters.AddWithValue("@gioitinh", gioitinh);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@sdt", sdt);
+            command.Parameters.AddWithValue("@chucvu", chucvu);
+            command.Parameters.AddWithValue("@id", id);
+            return Database.ExecuteNoneQuery(command);
+        }
+        public static int GiaoVien_Delete(string id)
+        {
+            string sql = "delete giaovien where magiaovien = @id";
+            SqlCommand command = new SqlCommand(sql);
+            command.Parameters.AddWithValue("@id", id);
+            return Database.ExecuteNoneQuery(command);
+        }
+    }
+}
---
+++ QuanLyTuyenSinh/QuanLyTuyenSinh/DB/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecec.DB
+{
+    class User
+    {
+        public static DataTable User_Login(string taikhoan, string matkhau)
+        {
+            string queryString = "SELECT * FROM taikhoan where tentaikhoan = @tentaikhoan and matkhau = @matkhau;";
+            SqlCommand command = new SqlCommand(queryString);
+            command.Parameters.Add("@tentaikhoan", SqlDbType.NVarChar, 50).Value = taikhoan;
+            command.Parameters.Add("@matkhau", SqlDbType.NVarChar, 255).Value = Hash(matkhau);
+            return Database.GetData(command);
+        }
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
